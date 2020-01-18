3d9e1e37de60c1ed10fc71bc42e18896eb998b3f
youngsw <sw_young@outlook.com>
2020-01-18 17:07:32 +0800

    add vimInstall

---
+++ SpaceVimSw.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+<<'COMMENT'
+COMMENT
+echo -e  "\033[31m 执行该脚本将会删除您之前安装的vim配置，如有需要，请妥善保存之前的配置文件。 \033[0m"
+
+yes="yes"
+read -p "确认继续安装吗 [Y/n]" input
+
+#判断用户输入，是否继续执行安装
+if [ "$input" = "Y" ];then
+	echo "继续安装>>>"
+else 
+	echo "退出安装..."
+	exit
+fi
+
+echo -e  "\033[32m 安装配置需要一定的时间，请耐心等待 \033[0m"
+#删除旧的配置文件
+sudo apt remove vim* -y
+sudo rm -rf ~/.vim*
+
+#安装依赖
+sudo apt install git -y
+sudo apt install curl -y
+sudo apt install vim -y
+sudo apt install clang-format -y
+sudo snap install clangd --classic
+#安装Spacevim
+curl -sLf https://spacevim.org/install.sh | bash
+
+
+sudo chmod 777 ~/.vim/
+sudo chmod 777 ~/.Vim*
+sudo chmod 777 ~/.SpaceVim*
+
+echo -e  "\033[32m vim命令行下可以试用SPUpdate进行插件更新 \033[0m"
+echo -e  "\033[32m SpaceVim的配置请参考 \033[0m"
+firefox https://spacevim.org/cn/quick-start-guide/ #url
+
+
